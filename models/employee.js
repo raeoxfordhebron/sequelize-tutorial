f7602b5de0e8d15a5826175bcbc323a91d35f93b
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-03-17 16:05:07 -0400

    employee and company models created

---
+++ models/employee.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Employee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Employee.init({
+    name: DataTypes.STRING,
+    designation: DataTypes.STRING,
+    salary: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Employee',
+  });
+  return Employee;
+};
